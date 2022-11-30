2438d986cab5839423cbfdb6290ca0f130a73064
Darsh <darshankondhare2698@gmail.com>
2022-11-30 15:35:50 +0530

    [darsh] Add:Check Validation First Name

---
+++ UserRegistrationProblemStatement/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace UserRegistrationProblemStatement
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Validation validation = new Validation();
+            Console.WriteLine("Enter The First Name: ");
+            string firstName = Console.ReadLine();
+            validation.FirstName(firstName);
+        }
+    }
+}

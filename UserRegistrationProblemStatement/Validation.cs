f5c3e1db51e7d32fc8adbbdec8a83307f2b4f9f7
Darsh <darshankondhare2698@gmail.com>
2022-11-30 15:43:53 +0530

    [darsh] Add:Check Validation Last Name

--- UserRegistrationProblemStatement/Validation.cs
+++ UserRegistrationProblemStatement/Validation.cs
@@ -10,11 +10,19 @@ namespace UserRegistrationProblemStatement
     internal class Validation
     {
         public const string FIRST_NAME_REGEX = "^[A-Z]{1}[a-zA-Z]{2}$";
+        public const string LAST_NAME_REGEX = "^[A-Z]{1}[a-zA-Z]{2}$";
         public void FirstName(string firstname)               //UC1-In FirstName the First Letter Should Capital and minimum 3 Character
         {
             Regex regex = new Regex(FIRST_NAME_REGEX);
             bool result = regex.IsMatch(firstname);
             Console.WriteLine(result);
         }
+        public void LastName(string lastname)
+        {
+            Regex regex = new Regex(LAST_NAME_REGEX);
+            bool result = regex.IsMatch(lastname);
+            Console.WriteLine(result);
+        }
     }
+
 }
